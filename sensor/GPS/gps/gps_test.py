3b70932c78a509eef5a75171f49f8827eb96a5dd
ujtk6014 <ujtk6014.m2@keio.jp>
2021-04-10 16:51:29 +0900

    gps_test

--- sensor/GPS/gps/gps_test.py
+++ sensor/GPS/gps/gps_test.py
@@ -3,26 +3,29 @@ import gps
 
 class cansat(object):
     def __init__(self):
-        self.gps = gps.GPS()
+        self.gps = gps.Gps()
 
     def setup(self):
-        self.gps.setupGPS()
+        self.gps.setupGps()
         
     def writeData(self):
         self.gps.gpsread()
         timer = 1000*(time.time() - start_time)
         timer = int(timer)
         datalog = str(timer) + ","\
-                  + str(self.gps.Time) + ","\
-                  + str(self.gps.Lat) + ","\
-                  + str(self.gps.Lon)
+                  + "Time:" + str(self.gps.Time) + ","\
+                  + "緯度:" + str(self.gps.Lat) + ","\
+                  + "経度:" + str(self.gps.Lon)
+        print(datalog)
     
+    '''
         with open("test.txt",mode = 'a') as test:
             test.write(datalog + '\n')
-
+'''
 
 start_time = time.time()
 cansat = cansat() 
 cansat.setup()
 while True:
-    cansat.writeData()   
+    cansat.writeData()
+    time.sleep(1)
