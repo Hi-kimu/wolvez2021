797ac326a6165e0318223b114cd784f1a5e752e2
Hi-kimu <63459574+Hi-kimu@users.noreply.github.com>
2021-05-21 14:07:20 +0900

    Update servomotor_test.py

--- sensor/Servomotor/servomotor_test.py
+++ sensor/Servomotor/servomotor_test.py
@@ -9,19 +9,9 @@ while True:
     try:
         print("servomotor run")
         servomotor.servo_angle(-90)               #サーボモータ -90°
-        #servomotor.servo_angle(-60)               #サーボモータ -60°
-        #servomotor.servo_angle(-30)               #サーボモータ -30°
         servomotor.servo_angle(0)                 #サーボモータ  0°
-        #servomotor.servo_angle(30)                #サーボモータ  30°
-        #servomotor.servo_angle(60)                #サーボモータ  60°
         servomotor.servo_angle(90)
-        '''
-        servomotor.servo_angle()
-        time.sleep(3)
-        servomotor.mid()
-        time.sleep(3)
-        servomotor.left()
-        '''
+        
     except KeyboardInterrupt:
         GPIO.cleanup()
         servomotor.stop()
