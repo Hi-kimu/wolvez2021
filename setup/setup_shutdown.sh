97d5f05fbf247d8337fe631116661c3aeb5cfa3b
Hi-kimu <63459574+Hi-kimu@users.noreply.github.com>
2021-06-30 15:14:59 +0900

    Update setup_shutdown.sh

--- setup/setup_shutdown.sh
+++ setup/setup_shutdown.sh
@@ -11,7 +11,7 @@ import RPi.GPIO as GPIO
 import os
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(25,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
     while True:
