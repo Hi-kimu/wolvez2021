4d97cec9bd650498074d5d1069556d662f9863bd
Okubo-56 <79041223+Okubo-56@users.noreply.github.com>
2021-07-31 10:23:21 +0900

    Update estimation_test.py

--- sensor/Encoder/estimation_test.py
+++ sensor/Encoder/estimation_test.py
@@ -14,6 +14,9 @@ y=0
 q=0
 del_t=0.2
 hantei = 0
+state = 1
+x_remind = []
+q_remind = []
 
 start_time=time.time()
 print("cansat-x :",x,"[m]")
@@ -36,28 +39,44 @@ try:
         print("cansat-x :",x,"[m]")
         print("cansat-y :",y,"[m]")
         print("cansat-q :",q,"[rad]")
+
 """
 try:
     print("motor run") 
-    MotorR.go(81)
-    MotorL.go(80)
-    
     while True:
-        t1=time.time()
-        cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
-        #time.sleep(del_t)
-        t2=time.time()
-        x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
-        print("cansat speed :",cansat_speed,"[m/s]")
-        print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
-        print("cansat-x :",x,"[m]")
-        print("cansat-y :",y,"[m]")
-        print("cansat-q :",q,"[rad]")
-        
-        if x >= 6:
-            print("motor run") 
+        if state == 1:
             MotorR.go(81)
-            MotorL.go(50)
+            MotorL.go(80)
+            t1=time.time()
+            cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
+            #time.sleep(del_t)
+            t2=time.time()
+            x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
+            print("cansat speed :",cansat_speed,"[m/s]")
+            print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
+            print("cansat-x :",x,"[m]")
+            print("cansat-y :",y,"[m]")
+            print("cansat-q :",q,"[rad]")
+            x_remind.append(x)
+            if x[-1]-x[0] >= 6:
+                state = 2
+        elif state == 2
+            print("motor curve") 
+            MotorR.go(81)
+            MotorL.go(0)
+            t1=time.time()
+            cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
+            #time.sleep(del_t)
+            t2=time.time()
+            x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
+            print("cansat speed :",cansat_speed,"[m/s]")
+            print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
+            print("cansat-x :",x,"[m]")
+            print("cansat-y :",y,"[m]")
+            print("cansat-q :",q,"[rad]")
+            q_remind.append(q)
+            if q[-1]-q[0] >=90:
+                state = 1
 """
 # try:
 #     print("motor run") 
