28be2bdf68386ef07c5689349424ffd7279aebe9
yu-chae <66954993+yu-chae@users.noreply.github.com>
2021-07-31 10:48:24 +0900

    Update radio.py

--- Testcode/Integration/radio.py
+++ Testcode/Integration/radio.py
@@ -81,8 +81,17 @@ class radio(object):
                     #print('-------------')
                     break
                     
-     def 
-                  
+     def estimate_distance_cansat(self,meancansatrssi,meanlostrssi)
+                #定義式より推定
+        N=2.8
+        MP=-42.2
+        cansatestimate_definition=10**((MP-meancansatrssi)/(10*N))
+        lostestimate_definition=10**((MP-meanlostrssi)/(10*N))
+                     self.cansatrssi.insert(2,str(cansatestimate_definition))
+         self.lostrssi.insert(2,str(lostestimate_definition))
+#         print('カンサット:定義式からの推定'+cansatestimate_definition)
+#         print('ロスト機:定義式からの推定'+lostestimate_definition)
+
 
         
         
