032d4ca8949796dfcc18cfce78c86447d761f28a
Okubo-56 <79041223+Okubo-56@users.noreply.github.com>
2021-08-04 14:33:31 +0900

    Update estimation_test.py

--- sensor/Encoder/estimation_test.py
+++ sensor/Encoder/estimation_test.py
@@ -33,84 +33,86 @@ print("cansat-x :",x,"[m]")
 print("cansat-y :",y,"[m]")
 print("cansat-q :",q,"[rad]")
 
-# try:
-#     print("motor run")
-#     MotorR.stop()
-#     MotorL.stop()
+try:
+    print("motor run")
+    MotorR.stop()
+    MotorL.stop()
     
 #     MotorR.go(81)
 #     MotorL.go(80)
-#     
-#     while True:
-#         t1=time.time()
-#         cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
-#         #time.sleep(del_t)
-#         t2=time.time()
-#         x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
-#         print("cansat speed :",cansat_speed,"[m/s]")
-#         print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
-#         print("cansat-x :",x,"[m]")
-#         print("cansat-y :",y,"[m]")
-#         print("cansat-q :",q,"[rad]")    
-bno055.bnoread()
-try:
-    print("motor run")
+    MotorR.back(60)
+    MotorL.back(60)
+    
     while True:
-        if state == 1:
-            q_remind=[]
-            MotorR.go(86.5)
-            MotorL.go(85)
-            t1=time.time()
-#             print(0)
-            cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
-            t2=time.time()
-            x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
-            bno055.bnoread()
-            q=radians(round(bno055.ex,3))
-            print("cansat speed :",cansat_speed,"[m/s]")
-            print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
-            print("cansat-x :",x,"[m]")
-            print("cansat-y :",y,"[m]")
-            print("cansat-q :",q,"[rad]")
-            print("cansat-q :",bno055.ex,"[degree]")
-            print("sin",sin(q))
-            x_remind.append(x)
-            y_remind.append(y)
-            if sqrt((abs(x_remind[-1]-x_remind[0]))**2 + (abs(y_remind[-1]-y_remind[0]))**2) >= 10:
-                state = 2
-        elif state == 2:
-            x_remind=[]
-            y_remind = []
-#             print("motor curve") 
-            MotorR.go(30)
-            MotorL.go(0)
-            t1=time.time()
-            cansat_speed,cansat_rad_speed=Encoder.est_v_w_for_c(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
-            t2=time.time()
-            x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
-            bno055.bnoread()
-            q=round(bno055.ex,6)
-            print("cansat speed :",cansat_speed,"[m/s]")
-            print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
-            print("cansat-x :",x,"[m]")
-            print("cansat-y :",y,"[m]")
-            print("cansat-q :",q,"")
-            print("sin",sin(radians(q)))
-            q_remind.append(radians(q))
-            """
-            if sin((abs(q_remind[-1]-q_remind[0])))>=0.9:
-                state = 1
-                """
-            if fabs(q-270)<1:
-                state=1
-        elif state == 3:
-            MotorR.go(81)
-            MotorL.go(80)
+        t1=time.time()
+        cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
+        #time.sleep(del_t)
+        t2=time.time()
+        x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
+        print("cansat speed :",cansat_speed,"[m/s]")
+        print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
+        print("cansat-x :",x,"[m]")
+        print("cansat-y :",y,"[m]")
+        print("cansat-q :",q,"[rad]")    
+# bno055.bnoread()
+# try:
+#     print("motor run")
+#     while True:
+#         if state == 1:
+#             q_remind=[]
+#             MotorR.go(86.5)
+#             MotorL.go(85)
+#             t1=time.time()
+# #             print(0)
+#             cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
+#             t2=time.time()
+#             x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
+#             bno055.bnoread()
+#             q=radians(round(bno055.ex,3))
+#             print("cansat speed :",cansat_speed,"[m/s]")
+#             print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
+#             print("cansat-x :",x,"[m]")
+#             print("cansat-y :",y,"[m]")
+#             print("cansat-q :",q,"[rad]")
+#             print("cansat-q :",bno055.ex,"[degree]")
+#             print("sin",sin(q))
+#             x_remind.append(x)
+#             y_remind.append(y)
+#             if sqrt((abs(x_remind[-1]-x_remind[0]))**2 + (abs(y_remind[-1]-y_remind[0]))**2) >= 10:
+#                 state = 2
+#         elif state == 2:
+#             x_remind=[]
+#             y_remind = []
+# #             print("motor curve") 
+#             MotorR.go(30)
+#             MotorL.go(0)
+#             t1=time.time()
+#             cansat_speed,cansat_rad_speed=Encoder.est_v_w_for_c(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
+#             t2=time.time()
+#             x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
+#             bno055.bnoread()
+#             q=round(bno055.ex,6)
+#             print("cansat speed :",cansat_speed,"[m/s]")
+#             print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
+#             print("cansat-x :",x,"[m]")
+#             print("cansat-y :",y,"[m]")
+#             print("cansat-q :",q,"")
+#             print("sin",sin(radians(q)))
+#             q_remind.append(radians(q))
+#             """
+#             if sin((abs(q_remind[-1]-q_remind[0])))>=0.9:
+#                 state = 1
+#                 """
+#             if fabs(q-270)<1:
+#                 state=1
+#         elif state == 3:
+#             MotorR.go(81)
+#             MotorL.go(80)
             
 
 # try:
 #     print("motor run") 
-#     MotorR.go(82)
+#     MotorR.back(80)
 #     MotorL.go(80)
 #     
 #     while hantei == 0:
